2d7596f65598a4fc63f2cf976c2767f26c118cb6
Ale15207 <69678910+Ale15207@users.noreply.github.com>
2022-06-14 01:25:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -205,6 +205,18 @@ client.on("messageCreate", (message) => {
 })
 
 client.on("messageCreate", (message) => {
+    if (message.content == "back"){
+        message.reply("welcome back")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "Back"){
+        message.reply("welcome back")
+    }
+})
+
+client.on("messageCreate", (message) => {
     if (message.content == "sex"){
         message.reply("Ok bud chill out for a second we don't talk about this because  this is a gaming server not some sort of hub for you to stink, get the fuck out")
     }
