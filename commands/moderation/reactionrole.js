cc85d7dd0e31abdfcac4c8c26a300358b9a0bdb9
Ale15207 <69678910+Ale15207@users.noreply.github.com>
2022-09-02 18:43:52 +0200

    Update reactionrole.js

--- commands/moderation/reactionrole.js
+++ commands/moderation/reactionrole.js
@@ -48,7 +48,7 @@ module.exports = {
                 + `${dmsopenemoji} to indicate that your dms are open`;
                 + `${dmsclosedemoji} to indicate that your dms are closed`;
 
-        let messageEmbed = await channel.send(embed);
+        let messageEmbed = channel.send(embed);
         messageEmbed.react(boyemoji);
         messageEmbed.react(girlemoji);
         messageEmbed.react(leoemoji);
